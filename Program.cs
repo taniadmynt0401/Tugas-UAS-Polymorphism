1b168adc4fbca96d1dc24d316c22df4e9e63cd3d
Tania Damayanti <127859384+taniadmynt0401@users.noreply.github.com>
2023-07-15 13:58:45 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Polymorphism;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace TicketBookingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Meminta pengguna untuk memilih jenis tiket
+            Console.WriteLine("Pilih jenis tiket:");
+            Console.WriteLine("1. Tiket Reguler");
+            Console.WriteLine("2. Tiket VIP");
+            Console.Write("Pilihan: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            // Membuat objek TicketBooking berdasarkan jenis tiket yang dipilih
+            TicketBooking ticketBooking;
+            if (choice == 1)
+            {
+                ticketBooking = new RegularTicket();
+            }
+            else if (choice == 2)
+            {
+                ticketBooking = new VipTicket();
+            }
+            else
+            {
+                Console.WriteLine("Pilihan tidak valid. Program berakhir.");
+                return;
+            }
+
+            // Meminta pengguna untuk memasukkan informasi pemesanan tiket
+            Console.Write("Masukkan Nama: ");
+            string name = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu Vaksin: ");
+            string vaccinationCardNumber = Console.ReadLine();
+            Console.Write("Masukkan NIK: ");
+            string nik = Console.ReadLine();
+            Console.Write("Masukkan Nomor Telepon: ");
+            string phoneNumber = Console.ReadLine();
+            Console.Write("Masukkan Nomor Kartu: ");
+            string cardNumber = Console.ReadLine();
+
+            // Mengatur nilai properti menggunakan method polymorphism
+            ticketBooking.SetInfo(name, vaccinationCardNumber, nik, phoneNumber, cardNumber);
+
+            // Memanggil method BookTicket untuk memesan tiket
+            ticketBooking.BookTicket();
+        }
+    }
+}
---
+++ TicketBooking.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    abstract class TicketBooking
+    {
+        protected string name;
+        protected string vaccinationCardNumber;
+        protected string nik;
+        protected string phoneNumber;
+        protected string cardNumber;
+
+        public void SetInfo(string name, string vaccinationCardNumber, string nik, string phoneNumber, string cardNumber)
+        {
+            this.name = name;
+            this.vaccinationCardNumber = vaccinationCardNumber;
+            this.nik = nik;
+            this.phoneNumber = phoneNumber;
+            this.cardNumber = cardNumber;
+        }
+
+        public abstract void BookTicket();
+    }
+    class RegularTicket : TicketBooking
+    {
+        public override void BookTicket()
+        {
+            Console.WriteLine("Tiket Reguler berhasil dipesan untuk:");
+            Console.WriteLine("Nama: " + name);
+            Console.WriteLine("Nomor Kartu Vaksin: " + vaccinationCardNumber);
+            Console.WriteLine("NIK: " + nik);
+            Console.WriteLine("Nomor Telepon: " + phoneNumber);
+            Console.WriteLine("Nomor Kartu: " + cardNumber);
+        }
+    }
+    class VipTicket : TicketBooking
+    {
+        public override void BookTicket()
+        {
+            Console.WriteLine("Tiket VIP berhasil dipesan untuk:");
+            Console.WriteLine("Nama: " + name);
+            Console.WriteLine("Nomor Kartu Vaksin: " + vaccinationCardNumber);
+            Console.WriteLine("NIK: " + nik);
+            Console.WriteLine("Nomor Telepon: " + phoneNumber);
+            Console.WriteLine("Nomor Kartu: " + cardNumber);
+        }
+    }
+}
+
+
+
